0e5492bef5662e388c7123ef974defcf84564051
TonyCao <tonycao0918@gamil.com>
2023-12-05 00:18:37 +0800

    add DANCE-somogcn

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 # config.py
 
 config = {
-    'input_train_mod1': './sample_data/openproblems_bmmc_multiome_starter/openproblems_bmmc_multiome_starter.train_mod1.h5ad',
-    'input_train_mod2': './sample_data/openproblems_bmmc_multiome_starter/openproblems_bmmc_multiome_starter.train_mod2.h5ad',
-    'input_test_mod1': "./sample_data/openproblems_bmmc_multiome_starter/openproblems_bmmc_multiome_starter.test_mod1.h5ad",
-    'input_test_mod2': './sample_data/openproblems_bmmc_multiome_starter/openproblems_bmmc_multiome_starter.test_mod2.h5ad',
+    'input_train_mod1': './data/openproblems_bmmc_cite_phase2_rna/openproblems_bmmc_cite_phase2_rna.censor_dataset.output_train_mod1.h5ad',
+    'input_train_mod2': './data/openproblems_bmmc_cite_phase2_rna/openproblems_bmmc_cite_phase2_rna.censor_dataset.output_train_mod2.h5ad',
+    'input_test_mod1': "./data/openproblems_bmmc_cite_phase2_rna/openproblems_bmmc_cite_phase2_rna.censor_dataset.output_test_mod1.h5ad",
+    'input_test_mod2': './data/openproblems_bmmc_cite_phase2_rna/openproblems_bmmc_cite_phase2_rna.censor_dataset.output_test_mod2.h5ad',
+    'root': './data',
+    'subtask': 'openproblems_bmmc_cite_phase2_rna',
     'output': 'output/output.h5ad',
     'n_pcs': 4,
     'n_neighbors': 5,
